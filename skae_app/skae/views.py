2b7bc9864585c876e92ce23926678cb8b3282aa8
nyaribari <anyaribari@gmail.com>
2022-05-11 21:41:55 +0300

    Writing more views and building the app

--- skae_app/skae/views.py
+++ skae_app/skae/views.py
@@ -4,3 +4,53 @@ import re
 
 skae = Blueprint('skae', __name__)
 
+@skae.route('/')
+@skae.route('/login', methods=['GET', 'POST'])
+def login():
+    message = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        conn = sqlite3.connect('skae_database')
+        c = conn.cursor()
+        c.execute("SELECT * FROM users_table WHERE username = ? AND password = ?", (username, password))
+        acc = c.fetchone()
+        if acc:
+            session['loggedin'] = True
+            session['username'] = acc['username']
+            session['id'] = acc['id']
+            message = 'You are now logged in'
+            return redirect(url_for('skae.home'))
+        else:
+            message = 'Invalid Credentials. Please try again.'
+    return render_template('login.html', message=message)
+
+@skae.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('username', None)
+    session.pop('id', None)
+    return redirect(url_for('skae.login'))
+
+
+@skae.route('/register', methods=['GET', 'POST'])
+def register():
+    message = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        conn = sqlite3.connect('skae_database')
+        c = conn.cursor()
+        c.execute("SELECT * FROM users_table WHERE username = ?", (username,))
+        acc = c.fetchone()
+        if acc:
+            message = 'Username already exists.'
+        elif not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            message = 'Invalid email.'
+        else:
+            c.execute("INSERT INTO users_table (username, password, email) VALUES (?, ?, ?)", (username, password, email))
+            conn.commit()
+            message = 'You are now registered and can log in.'
+    return render_template('register.html', message=message)
+    
